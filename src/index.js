9f604c65068820d1b14e2ddcc6fc547d6f207146
Doaa Mahely <dmahely@hotmail.com>
2021-03-20 18:48:38 +0400

    Set up React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import { App } from './components/App.js';
+
+console.log("Let's get React working")
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ["@babel/env"]}
       }
     ]
   },
