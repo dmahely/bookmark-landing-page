2bd4862e363fc502220399a7d1c4645a4cced0dc
Doaa Mahely <dmahely@hotmail.com>
2021-03-29 21:18:49 +0400

    Add input validation

--- src/components/NewsletterForm/NewsletterForm.js
+++ src/components/NewsletterForm/NewsletterForm.js
@@ -7,8 +7,10 @@ const NewsletterForm = () => {
         <div className="Newsletter--container">
             <span className="Newsletter--heading">35,000+ already joined</span>
             <span className="Newsletter--description">Stay up-to-date with what we're doing</span>
-            <input className="Newsletter--input" type="text" placeholder="Enter your email address" />
-            <TextButton text="Contact Us" color="white" size="l" backgroundColor="red" />
+            <form>
+                <input required className="Newsletter--input" type="email" placeholder="Enter your email address" />
+                <TextButton text="Contact Us" color="white" size="l" backgroundColor="red" />
+            </form>
         </div>
     );
 }
