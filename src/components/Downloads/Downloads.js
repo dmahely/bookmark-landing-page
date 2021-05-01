c6c14d592b5f0781355bdad9089fb6ed57daaf5c
Doaa Mahely <dmahely@hotmail.com>
2021-05-02 01:21:29 +0400

    Update Downloads component styling

--- src/components/Downloads/Downloads.js
+++ src/components/Downloads/Downloads.js
@@ -7,25 +7,34 @@ import chromeIcon from "../../../images/logo-chrome.svg";
 
 const Downloads = () => {
   return (
-    <div className="Downloads--container">
-      <BrowserDownload
-        browser="Chrome"
-        minVersion="62"
-        icon={chromeIcon}
-        num="1"
-      />
-      <BrowserDownload
-        browser="Firefox"
-        minVersion="55"
-        icon={firefoxIcon}
-        num="2"
-      />
-      <BrowserDownload
-        browser="Opera"
-        minVersion="46"
-        icon={operaIcon}
-        num="3"
-      />
+    <div className="Downloads--outer-container">
+      <div className="Downloads--text-container">
+        <h3 className="Downloads--heading">Download the extension</h3>
+        <p className="Downloads--description">
+          We've got more browsers in the pipeline. Please do let us know if
+          you’ve got a favourite you’d like us to prioritize.
+        </p>
+      </div>
+      <div className="Downloads--container">
+        <BrowserDownload
+          browser="Chrome"
+          minVersion="62"
+          icon={chromeIcon}
+          num="1"
+        />
+        <BrowserDownload
+          browser="Firefox"
+          minVersion="55"
+          icon={firefoxIcon}
+          num="2"
+        />
+        <BrowserDownload
+          browser="Opera"
+          minVersion="46"
+          icon={operaIcon}
+          num="3"
+        />
+      </div>
     </div>
   );
 };
