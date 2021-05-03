92e1079561dca9ef8e7774038c5c39edf60723d5
Doaa Mahely <dmahely@hotmail.com>
2021-05-03 23:47:50 +0400

    Add base changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import "./style.css";
-import React from 'react';
-import ReactDOM  from 'react-dom';
-import { App } from './components/App.js';
-
-console.log("Let's get React working")
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
