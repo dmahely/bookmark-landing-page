d38dbc1fd799ef28daf088b1abe6fc6c12130524
Doaa Mahely <dmahely@hotmail.com>
2021-05-23 23:44:39 +0400

    Move attribution to Footer component

--- src/components/App.js
+++ src/components/App.js
@@ -27,16 +27,6 @@ const App = () => {
         <NewsletterForm />
         <Footer />
       </main>
-
-      <footer>
-        <span className="attribution">
-          Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-            Frontend Mentor
-          </a>
-          . Coded by <a href="https://github.com/dmahely">Doaa Mahely</a>.
-        </span>
-      </footer>
     </Fragment>
   );
 };
